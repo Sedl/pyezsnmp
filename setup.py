aa3be9104f32dd3260dd04f7eac78b0224a264e3
Stephan Sedlmeier <sedlmeier@komro.net>
2016-05-02 16:35:01 +0200

    Added missing package ezsnmp.devices in setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ REQUIRES = [line.strip() for line in open('requirements.txt') if line.strip()]
 setup(
     name='ezsnmp',
     version='0.1',
-    packages=['ezsnmp'],
+    packages=['ezsnmp', 'ezsnmp.devices'],
     install_requires=REQUIRES,
     url='https://github.com/Sedl/pyezsnmp'
 )
